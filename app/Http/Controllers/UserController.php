5755c8960e4c34605c15e90d4e9be33df3ffe4c0
minh <minh@lastvoices.com>
2018-05-24 22:59:07 +0700

    #6 - return success string

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,9 +8,11 @@ class UserController extends Controller
 {
     public function index(Request $request)
     {
-        echo $request->ip();
+        //echo $request->ip();
+        //get value and aupdate db here
         $urlQueryString = $request->query();
 
-        return response()->json($urlQueryString, 200);
+        return "0 | Ban da dang ky tai khoan thanh cong";
+        //return response()->json($urlQueryString, 200);
     }
 }
