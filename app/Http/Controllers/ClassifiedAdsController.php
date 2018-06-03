f4f9a861be217f7fd262cc48e52d341f66aef576
minhvong1810 <32826381+minhvong1810@users.noreply.github.com>
2018-06-03 17:01:02 +0700

    #16 - show data

--- app/Http/Controllers/ClassifiedAdsController.php
+++ app/Http/Controllers/ClassifiedAdsController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\ClassifiedAds;
 
 class ClassifiedAdsController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function getAll()
+    {
+        $title = 'Home';
+        $classifiedAds = ClassifiedAds::all();
+
+        $data = [
+            'title' => $title,
+            'classifiedAds' => $classifiedAds
+        ];
+
+        return view('home', $data);
+    }
 }
